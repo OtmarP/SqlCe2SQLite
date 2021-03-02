14f1a6c2d7e04755ba624e56678f60e004a35897
OtmarP (opMedion14b) <Otmar.Pilgerstorfer@chello.at>
2021-03-02 17:44:50 +0100

    Init Project/Solution

---
+++ src/SqlCe2SQLite/SqlCe2SQLiteMain.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace SqlCe2SQLite
+{
+    partial class SqlCe2SQLiteMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // SqlCe2SQLiteMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 328);
+            this.Name = "SqlCe2SQLiteMain";
+            this.Text = "SqlCe2SQLite";
+            this.Load += new System.EventHandler(this.SqlCe2SQLiteMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ src/SqlCe2SQLite/SqlCe2SQLiteMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlCe2SQLite
+{
+    public partial class SqlCe2SQLiteMain : Form
+    {
+        public SqlCe2SQLiteMain()
+        {
+            InitializeComponent();
+
+            this.Top = 50;
+            this.Left = 50;
+        }
+
+        private void SqlCe2SQLiteMain_Load(object sender, EventArgs e)
+        {
+            //
+        }
+    }
+}
