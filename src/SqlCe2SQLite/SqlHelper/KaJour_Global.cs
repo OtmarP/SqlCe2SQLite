f5675f20dad24dedd7764c2b24bb7f817345efbf
Otmar Pilgerstorfer (chello, OP-Surface-Book) <OtmarPil@outlook.com>
2021-03-06 19:52:59 +0100

    remove KaJour_Global Vars

--- src/SqlCe2SQLite/SqlHelper/KaJour_Global.cs
+++ src/SqlCe2SQLite/SqlHelper/KaJour_Global.cs
@@ -9,36 +9,37 @@ namespace KaJourDAL
 {
     public static class KaJour_Global
     {
-        // Instance-Variables
-        private static string m_globalVar = "";
-        private static string m_sys_prog = "";
-        //public static string SQLProvider;
+        //// Instance-Variables
+        //private static string m_globalVar = "";
+        //private static string m_sys_prog = "";
+        ////public static string SQLProvider;
 
-        // Properties
-        public static string GlobalVar
-        {
-            get { return m_globalVar; }
-            set { m_globalVar = value; }
-        }
+        //// Properties
+        //public static string GlobalVar
+        //{
+        //    get { return m_globalVar; }
+        //    set { m_globalVar = value; }
+        //}
         
-        public static string sys_prog
-        {
-            get { return m_sys_prog; }
-            set { m_sys_prog = value; }
-        }
-        public static string sys_rzmand { get; set; }
-        /// <summary>
-        /// v:1.0.20200101.1730
-        /// </summary>
-        public static string sys_ver { get; set; }
-        public static string prg_back { get; set; }
-        public static string stxt_auth { get; set; }
-        public static bool prg_man { get; set; }
-        //
-        public static string MandNummer { get; set; }
-        //
-        public static string SQLProvider { get; set; }
-        public static string SQLConnStr { get; set; }
+        //public static string sys_prog
+        //{
+        //    get { return m_sys_prog; }
+        //    set { m_sys_prog = value; }
+        //}
+        //public static string sys_rzmand { get; set; }
+        ///// <summary>
+        ///// v:1.0.20200101.1730
+        ///// </summary>
+        //public static string sys_ver { get; set; }
+        //public static string prg_back { get; set; }
+        //public static string stxt_auth { get; set; }
+        //public static bool prg_man { get; set; }
+        ////
+        //public static string MandNummer { get; set; }
+
+        ////
+        //public static string SQLProvider { get; set; }
+        //public static string SQLConnStr { get; set; }
 
         // ####################
         // Methods
