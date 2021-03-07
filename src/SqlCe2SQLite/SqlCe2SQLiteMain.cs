51b9bb0aadc1ffaa719c1896de4888b73bc13ea9
OtmarP (opMedion14b) <Otmar.Pilgerstorfer@chello.at>
2021-03-07 15:27:10 +0100

    Display Record# with modulo

--- src/SqlCe2SQLite/SqlCe2SQLiteMain.cs
+++ src/SqlCe2SQLite/SqlCe2SQLiteMain.cs
@@ -284,6 +284,53 @@ namespace SqlCe2SQLite
                 var tableSelect = sqlCe.Execute("SELECT", "SELECT * FROM " + tableName);
                 for (int iRow = 0; iRow < tableSelect.Rows.Count; iRow++)
                 {
+                    this.toolStripStatusLabel2.Text = " " + (iTable + 1).ToString() + "/" + tablesCE.Rows.Count.ToString() + " " + tableName + " " + (iRow + 1).ToString() + "/" + tableRec1.ToString() + " ";
+                    // 1,2,3,4,5,6,7,8,9,10
+                    // - 20,30,40,50,60,70,80,90,100
+                    // - 200,300,400,500,600,700,800,900,1000
+                    // - 2000,3000,4000,5000,6000,7000,8000,9000,10000
+                    // - 20000,30000,40000,50000,60000,70000, ...
+                    bool doEvents = false;
+                    var iRowP1 = iRow + 1;
+                    if (iRowP1 <= 10) {
+                        doEvents = true;
+                    }
+                    else if (iRowP1 <= 100)
+                    {
+                        // mod 10
+                        if ((iRowP1 % 10) == 0) { doEvents = true; }
+                    }
+                    else if (iRowP1 <= 1000)
+                    {
+                        // mod 100
+                        if ((iRowP1 % 100) == 0) { doEvents = true; }
+                    }
+                    else if (iRowP1 <= 10000)
+                    {
+                        // mod 1000
+                        if ((iRowP1 % 1000) == 0) { doEvents = true; }
+                    }
+                    else if (iRowP1 <= 100000)
+                    {
+                        // mod 10000
+                        if ((iRowP1 % 10000) == 0) { doEvents = true; }
+                    }
+                    else if (iRowP1 <= 1000000)
+                    {
+                        // mod 100000
+                        if ((iRowP1 % 100000) == 0) { doEvents = true; }
+                    }
+                    else if (iRowP1 <= 10000000)
+                    {
+                        // mod 1000000
+                        if ((iRowP1 % 1000000) == 0) { doEvents = true; }
+                    }
+
+                    //
+                    if (doEvents) {
+                        Application.DoEvents();
+                    }
+
                     // Test
                     if (testNRecords)
                     {
